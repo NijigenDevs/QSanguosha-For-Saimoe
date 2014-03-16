04b89708a9178679b9eddace8aac15a6772d4ff8
SwordElucidator <nagisa940216@gmail.com>
2014-03-16 00:21:04 -0700

    Add moesen package and some translations. Now skills can be added.

---
+++ src/package/animation.h
@@ -0,0 +1,7 @@
+#ifndef _ANIMATION_H
+#define _ANIMATION_H
+
+#include "moesen-package.h"
+#include "card.h"
+
+#endif
---
+++ src/package/game.h
@@ -0,0 +1,7 @@
+#ifndef _GAME_H
+#define _GAME_H
+
+#include "moesen-package.h"
+#include "card.h"
+
+#endif
