6c225d6b3e6583cb8aa4cbc71e37bde1e12de185
SURFACEPRO3\Mitch <acgkiller@live.com>
2017-04-27 00:02:20 +0800

    Remove Some Images

--- hero-skin/tianfeng/1/zh_CN.lua
+++
@@ -1,5 +0,0 @@
-sgs.LoadSkinTransltionTable {
-	["cv:tianfeng"] = "HeLi",
-	["$suishi1"] = "我军大胜，吾性命无忧矣。",
-	["$suishi2"] = "我军大败，吾必死矣。",
-}
--- hero-skin/zangba/1/zh_CN.lua
+++
@@ -1,6 +0,0 @@
-sgs.LoadSkinTransltionTable {
-	["cv:zangba"] = "老狼",
-	["$hengjiang1"] = "横江断人道，骁骑跨江表。",
-	["$hengjiang2"] = "执义征暴，清定海岱。",
-	["~zangba"] = "明公不识霸言耳……",
-}
