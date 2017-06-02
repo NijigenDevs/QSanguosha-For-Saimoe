de54c54423e91209344d5ee7eae6711f9a33eeb8
Ryzen\OmnisReen <acgkiller@live.com>
2017-06-02 14:54:06 +0800

    [0.2.6] Version Number Updates

--- src/core/engine.cpp
+++ src/core/engine.cpp
@@ -637,7 +637,7 @@ SkillCard *Engine::cloneSkillCard(const QString &name) const
 
 QSanVersionNumber Engine::getVersionNumber() const
 {
-    return QSanVersionNumber(0, 2, 5);
+    return QSanVersionNumber(0, 2, 6);
 }
 
 QString Engine::getVersion() const
