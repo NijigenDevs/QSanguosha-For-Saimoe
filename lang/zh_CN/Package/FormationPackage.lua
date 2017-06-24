9eb7017558eb58a85115946315954762bd9e35ff
Ryzen\OmnisReen <acgkiller@live.com>
2017-06-24 16:56:39 +0800

    DPR Translation

--- lang/zh_CN/Package/FormationPackage.lua
+++ lang/zh_CN/Package/FormationPackage.lua
@@ -6,5 +6,7 @@ return {
 	[":DragonPhoenix"] = "装备牌·武器\n\n攻击范围：2\n技能：每当你使用【杀】指定一个目标后，你可以令其弃置一张牌；每当被你使用【杀】杀死的角色死亡后，若你的势力是角色最少的势力，你可以令扮演其的玩家选择是否从未使用的武将牌中选择与你势力相同的一张武将牌重新加入游戏。",
 	["@dragonphoenix-discard"] = "受到【飞龙夺凤】效果影响，请弃置一张牌",
 	["DragonPhoenix:revive"] = "你可以点击下方确定按钮重新复活加入战斗",
-
+	["DragonPhoenixRevive0"] = "%from 复活加入战斗",
+	["DragonPhoenixRevive1"] = "%from 移除了 %arg，复活加入战斗",
+	["DragonPhoenixRevive2"] = "%from 移除了 %arg 和 %arg2，复活加入战斗",
 }
